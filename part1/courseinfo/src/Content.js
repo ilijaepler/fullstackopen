9e2ba9ca7a9d3a0f8a39f28756170a88fd7a241d
ilijaepler <eplerilija17@gmail.com>
2022-08-05 21:47:48 +0200

    part1 exercises 1.3-1.5

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,9 +1,12 @@
 import Part from "./Part"
 
 const Content = (props) => {
+    console.log(props)
     return (
         <>
-           <Part part={props.part} exercises={props.exercises}/>
+           <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises}/>
+           <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises}/>
+           <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises}/>
         </>
     )
 }
