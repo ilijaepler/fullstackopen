cbf1c032b6592036bc64f7684ae8fdcb348984cc
ilijaepler <eplerilija17@gmail.com>
2022-08-05 20:44:57 +0200

    Part1 exercises 1.1, 1.2

---
+++ part1/courseinfo/src/Content.js
@@ -0,0 +1,11 @@
+import Part from "./Part"
+
+const Content = (props) => {
+    return (
+        <>
+           <Part part={props.part} exercises={props.exercises}/>
+        </>
+    )
+}
+
+export default Content
